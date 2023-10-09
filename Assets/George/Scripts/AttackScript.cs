e9e57e42e7b0071cfe2eef6af246a96a750a8107
george <115006794+thegeorgehewitt@users.noreply.github.com>
2023-10-09 16:35:21 +0700

    AttackScript Created
    Basic Enemy Detection Set Up with New Input System

--- Assets/George/Scripts/AttackScript.cs
+++ Assets/George/Scripts/AttackScript.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AttackScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public PlayerInputActions playerControls;
+    private InputAction meleeAttack;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        meleeAttack = playerControls.Player.Attack;
+        meleeAttack.Enable();
+        meleeAttack.performed += Attack;
+    }
+
+    private void OnDisable()
+    {
+        meleeAttack.Disable();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Attack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack");
+
+        // play attack animation
+        //
+
+        // detect enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // damage enemies
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("enemy hit: " + enemy.name);
+        }
     }
 }
