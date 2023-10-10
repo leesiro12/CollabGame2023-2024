289d6271917f5fced495283eb6e4320b0aa800dc
L4ppy <laphuynh1710@gmail.com>
2023-10-11 02:52:05 +0700

    DOTween added
    more convenience to do transforms

---
+++ Assets/Lap/Scripts/anythingAboutEnemy/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float walkingDistance;
+    [SerializeField] private float walkingSpeed;
+    [SerializeField] private Transform tform;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+
+    }
+}
