9ddbc500bc598929a44df7f057cc608045f7695b
komacholl <146920225+komacholl@users.noreply.github.com>
2023-10-11 14:48:42 +0700

    Advanced movement start

--- Assets/Lap/Scripts/SimpleMovement.cs
+++ Assets/Lap/Scripts/SimpleMovement.cs
@@ -9,6 +9,14 @@ public class SimpleMovement : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundLayer;
 
+    //dash variables
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashPower = 24f;
+    private float dashTime = 0.2f;
+    private float dashCooldown = 1f;
+    [SerializeField] private TrailRenderer tr;
+
 
     private float horizontal;
     private float speed = 8f;
@@ -20,6 +28,11 @@ public class SimpleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
+
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
         if (!isFacingRight && horizontal > 0f)
@@ -31,6 +44,8 @@ public class SimpleMovement : MonoBehaviour
             Flip();
         }
     }
+
+    
 
     public void Jump(InputAction.CallbackContext context)
     {
@@ -61,5 +76,26 @@ public class SimpleMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
