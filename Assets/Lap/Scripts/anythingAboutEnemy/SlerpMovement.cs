ad707970a5ba55c6e57d0c2005d3bf1c62a2cbb8
L4ppy <laphuynh1710@gmail.com>
2023-11-28 21:29:18 +0700

    Week 9 jump enemy task

--- Assets/Lap/Scripts/anythingAboutEnemy/SlerpMovement.cs
+++ Assets/Lap/Scripts/anythingAboutEnemy/SlerpMovement.cs
@@ -5,34 +5,31 @@ using UnityEngine;
 public class SlerpMovement : MonoBehaviour
 {
     [Header("Enemy Info")]    
-    [SerializeField] private float walkingSpeed;
-    [SerializeField] public GameObject leftLimit;
-    [SerializeField] public GameObject rightLimit;
-    [SerializeField] public Vector3 leftLandingPoint;
-    [SerializeField] public Vector3 rightLandingPoint;
-    [SerializeField] public Vector3 leftUpperPoint;
-    [SerializeField] public Vector3 rightUpperPoint;
-    private Rigidbody2D rb;
-    private Transform currentPoint; // show the next patrol point that enemy is moving to
-    private bool isFacingRight = true;
-    private bool isAttacking = false;
+    //[SerializeField] private float walkingSpeed;
+    
+    private Rigidbody2D rb;    
     private bool isCharging = false;
     private bool enemyPresent = true;
-    private Coroutine attackCoroutine;
+    private Coroutine attackCoroutine;  
+
+
+    [Header("Bezier Curve Points")]
+    public Transform startPoint;
+    public Transform controlPoint1;
+    public Transform controlPoint2;
+    public Transform endPoint;
+
 
     //Player informations
     [Header("Player")]
     public Transform playerTransform;
     public bool isChasing;
-    public float detectRange;
     public float chaseSpeed;
 
     public float attackRange;
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        currentPoint = rightLimit.transform;
-        //playerTransform = GameObject.FindGameObjectsWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();        
     }
 
 
@@ -44,19 +41,22 @@ public class SlerpMovement : MonoBehaviour
         {
             enemyPresent = true;
 
-            if(attackCoroutine == null)
+            if (attackCoroutine == null)
             {
+                Debug.Log("Attacking");
 
-                leftLandingPoint = transform.position * transform.localScale.x;
-                leftUpperPoint = new Vector3(transform.position.x + 1f, transform.position.y + 3.5f, transform.position.z) * transform.localScale.x;
-                rightUpperPoint = new Vector3(transform.position.x + 3f, transform.position.y + 3.5f, transform.position.z) * transform.localScale.x;
-                rightLandingPoint = new Vector3(transform.position.x + attackRange, transform.position.y, transform.position.z) * transform.localScale.x; 
+                // Update Bezier curve points
+                startPoint.position = transform.position;
+                controlPoint1.position = transform.position + Vector3.up * 2f; // Adjust as needed
+                controlPoint2.position = other.transform.position + Vector3.up * 2f; // Adjust as needed
+                endPoint.position = other.transform.position;
 
-                attackCoroutine = StartCoroutine(ChargePlayer(leftLandingPoint, leftUpperPoint, rightUpperPoint, rightLandingPoint, 1f));
+                attackCoroutine = StartCoroutine(ChargePlayer(startPoint.position, controlPoint1.position, controlPoint2.position, endPoint.position, 3f));
             }
 
             UpdateDirection(other);
         }
+        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -65,6 +65,7 @@ public class SlerpMovement : MonoBehaviour
 
         if (script != null)
         {
+            Debug.Log("Staying");
             UpdateDirection(collision);
         }
     }
@@ -77,21 +78,12 @@ public class SlerpMovement : MonoBehaviour
         {
             // marks player is not in trigger area
             enemyPresent = false;
+            Debug.Log("enemy stop presenting");
+            //StopCoroutine(attackCoroutine);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (attackCoroutine != null)
-        {
-            // stop the coroutine form running
-            StopCoroutine(attackCoroutine);
-        }
-
-        //warning.SetActive(false);
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
     {
         // get health script
         HealthScript script = collision.gameObject.GetComponent<HealthScript>();
@@ -115,6 +107,18 @@ public class SlerpMovement : MonoBehaviour
             // start coroutine for timed attack and hold reference in coroutine variable
             attackCoroutine = StartCoroutine(TimedAttack(collision));
         }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (attackCoroutine != null)
+        {
+            Debug.Log("Player detected");
+            // stop the coroutine form running
+            StopCoroutine(attackCoroutine);
+        }
+
     }
 
     void UpdateDirection(Collider2D collider)
@@ -146,53 +150,94 @@ public class SlerpMovement : MonoBehaviour
                 Debug.Log("dealt 10 damage (repeat)");
             } 
         }
+        else
+        {
+            yield return null;
+        }
                
     }
     
-    IEnumerator ChargePlayer(Vector3 p0, Vector2 p1, Vector2 p2, Vector3 p3, float t)
+    IEnumerator ChargePlayer(Vector3 p0, Vector2 p1, Vector2 p2, Vector3 p3, float duration)
     {
-        while (enemyPresent)
+        //while (enemyPresent)
+        //{
+        //    yield return new WaitForSeconds(1f);
+
+        //    Vector3 a = Vector3.Lerp(p0, p1, t * Time.deltaTime);
+        //    Vector3 b = Vector3.Lerp(p1, p2, t * Time.deltaTime);
+        //    Vector3 c = Vector3.Lerp(p2, p3, t * Time.deltaTime);
+        //    Vector3 d = Vector3.Lerp(a, b, t * Time.deltaTime);
+        //    Vector3 e = Vector3.Lerp(b, c, t * Time.deltaTime);
+        //    transform.position = Vector3.Lerp(d, e, t * Time.deltaTime);
+
+
+
+       
+        //    Debug.Log("Is charging");
+
+        //    isCharging = true;
+        //    yield return new WaitForSeconds(3f);
+        //    isCharging = false;
+        //}
+
+        //attackCoroutine = null;
+
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration && enemyPresent == true)
         {
-            yield return new WaitForSeconds(1f);
+            float t = timeElapsed / duration;
 
-            Vector3 a = Vector3.Lerp(p0, p1, t * Time.deltaTime);
-            Vector3 b = Vector3.Lerp(p1, p2, t * Time.deltaTime);
-            Vector3 c = Vector3.Lerp(p2, p3, t * Time.deltaTime);
-            Vector3 d = Vector3.Lerp(a, b, t * Time.deltaTime);
-            Vector3 e = Vector3.Lerp(b, c, t * Time.deltaTime);
-            Vector3 updatePos = Vector3.Lerp(d, e, t * Time.deltaTime);
+            // Calculate position on the Bezier curve using Bernstein polynomial
+            Vector3 newPosition = CalculateBezierPoint(p0, p1, p2, p3, t);
 
-            transform.position = updatePos;
-            Debug.Log("Is charging");
+            // Move the enemy to the new position
+            transform.position = newPosition;
 
-            isCharging = true;
-            yield return new WaitForSeconds(3f);
-            isCharging = false;
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
 
         attackCoroutine = null;
     }
 
+    Vector3 CalculateBezierPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        // Bezier curve equation
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+
+        return p;
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(leftLimit.transform.position, 0.5f);
-        Gizmos.DrawWireSphere(rightLimit.transform.position, 0.5f);
-        Gizmos.DrawLine(leftLimit.transform.position, rightLimit.transform.position);
+        //Gizmos.DrawWireSphere(leftLimit.transform.position, 0.5f);
+        //Gizmos.DrawWireSphere(rightLimit.transform.position, 0.5f);
+        //Gizmos.DrawLine(leftLimit.transform.position, rightLimit.transform.position);
 
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(this.transform.position, detectRange);
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawWireSphere(this.transform.position, detectRange);
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(this.transform.position, attackRange);
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(this.transform.position, attackRange);
 
-        Gizmos.color = Color.cyan;        
-        Gizmos.DrawWireSphere(leftLandingPoint, 0.5f);
-        Gizmos.DrawWireSphere(rightLandingPoint, 0.5f);
-        Gizmos.DrawLine(leftLandingPoint, rightLandingPoint);
+        //Gizmos.color = Color.cyan;        
+        //Gizmos.DrawWireSphere(leftLandingPoint, 0.5f);
+        //Gizmos.DrawWireSphere(rightLandingPoint, 0.5f);
+        //Gizmos.DrawLine(leftLandingPoint, rightLandingPoint);
 
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(leftUpperPoint, 0.5f);
-        Gizmos.DrawWireSphere(rightUpperPoint, 0.5f);
+        //Gizmos.DrawWireSphere(leftUpperPoint, 0.5f);
+        //Gizmos.DrawWireSphere(rightUpperPoint, 0.5f);
         Gizmos.DrawWireSphere(Vector3.left, 0.5f);
     }
 }
