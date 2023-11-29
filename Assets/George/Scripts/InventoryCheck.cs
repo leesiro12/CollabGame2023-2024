ef9c3c119b2db660b0c0e03f31ee02146610ae55
george <115006794+thegeorgehewitt@users.noreply.github.com>
2023-11-29 16:35:32 +0700

    Unlockable Door and Key
    Prefabs placeable, example in prototype level.

---
+++ Assets/George/Scripts/InventoryCheck.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryCheck : MonoBehaviour
+{
+    // track if key is held
+    private bool hasKey = false;
+
+    // check value
+    public bool GetKeyStatus()
+    {
+        return hasKey;
+    }
+
+    // change value
+    public void SetKeyStatus(bool value)
+    {
+        hasKey = value;
+    }
+}
