a0869e3ffe5330cc8e9ff454dcee1d16ae9e0bce
leesiro12 <104563243+leesiro12@users.noreply.github.com>
2024-04-29 10:19:10 +0700

    Asset culling Audio files

---
+++ Assets/D.Minh/Scripts/MAudioManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAudioManager : MonoBehaviour
+{
+    public static MAudioManager instance;
+    public AudioSource[] SFX, music;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        SFX = GetComponents<AudioSource>();
+    }
+
+    public void PlaySFX(AudioClip audioClip, float volume, Transform transform)
+    {
+        AudioSource audioSource = Instantiate(SFX, transform.position, Quaternion.identity);
+    }
+
+    public void StopSounds()
+    {
+        foreach (AudioSource audioSource in SFX)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        foreach (AudioSource audioSource in SFX)
+        {
+            audioSource.volume = volume;
+        }
+    }
+}
