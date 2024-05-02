40d676ed084be488c6a098a7496fde6fc666ed21
george <115006794+thegeorgehewitt@users.noreply.github.com>
2024-05-02 11:30:22 +0700

    New charge enemy type (patrol issues)

--- Assets/Hao/Scripts/PlayerManager.cs
+++ Assets/Hao/Scripts/PlayerManager.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     // the default check point is the starting position of the current level
-    public static Vector2 defaultCheckPointPos = new Vector2(18, 3);
+    public static Vector2 defaultCheckPointPos = new Vector2(-19, -22);
 
     // the last check point is the position of the latest red marker that player touches
-    public static Vector2 lastCheckPointPos = new Vector2(18, 3);
+    public static Vector2 lastCheckPointPos = new Vector2(-19, -22);
 
     private void Awake()
     {
