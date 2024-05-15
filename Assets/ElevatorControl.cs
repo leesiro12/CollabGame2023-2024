8514ee502131a31506a8e75293085736c8944a5c
L4ppy <114556422+L4ppy@users.noreply.github.com>
2024-05-16 05:50:52 +0700

    Elevator Fix update

---
+++ Assets/ElevatorControl.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorControl : MonoBehaviour
+{
+    public Transform[] points;
+    public float speed = 2.0f;
+    private int currentPointIndex = 1;
+    private bool reverse = false;
+    private bool isMoving = false;
+    Transform targetPoint;
+
+    private void Awake()
+    {
+        transform.position = points[0].position;        //Set platform to first point position
+    }
+    void Update()
+    {
+        if (isMoving)
+        {
+            MovePlatform();
+        }
+    }
+
+    public void StartMoving()       //player will interact thourgh this bool
+    {
+        if (!isMoving && points.Length > 0)
+        {
+            isMoving = true;
+        }
+    }
+
+    private void MovePlatform()
+    {
+        if (points.Length == 0)
+            return;
+        targetPoint = points[currentPointIndex];    //Set target Position
+        float step = speed * Time.deltaTime;        //Moving speed
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, step);
+
+        if (Vector3.Distance(transform.position, targetPoint.position) < 0.001f)    //Check To update next target
+        {
+            isMoving = false;
+            UpdateNextPointIndex();
+        }
+    }
+
+    private void UpdateNextPointIndex()
+    {
+        if (!reverse)
+        {
+            if (currentPointIndex < points.Length - 1)
+            {
+                currentPointIndex++;
+            }
+            else
+            {
+                reverse = true;
+                currentPointIndex--;
+            }
+        }
+        else
+        {
+            if (currentPointIndex > 0)
+            {
+                currentPointIndex--;
+            }
+            else
+            {
+                reverse = false;
+                currentPointIndex++;
+            }
+        }
+    }
+}
