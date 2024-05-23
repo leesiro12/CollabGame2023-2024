06c55721b526f107697eabcd5913029dc7cad6a9
leesiro12 <104563243+leesiro12@users.noreply.github.com>
2024-05-23 12:09:23 +0700

    Audio Manager

--- Assets/D.Minh/Scripts/MAudioManager.cs
+++ Assets/D.Minh/Scripts/MAudioManager.cs
@@ -1,46 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MAudioManager : MonoBehaviour
 {
     public static MAudioManager instance;
-    public AudioSource[] SFX, music;
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+    public SoundScript[] musicClips;
+    public SoundScript[] SFXClips;
+
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
-            return;
         }
-
-        DontDestroyOnLoad(gameObject);
-
-        SFX = GetComponents<AudioSource>();
     }
-
-    public void PlaySFX(AudioClip audioClip, float volume, Transform transform)
+    private void Start()
     {
-        //AudioSource audioSource = Instantiate(SFX, transform.position, Quaternion.identity);
+        
     }
-
-    public void StopSounds()
+    public void PlayMusic(string clipName)
     {
-        foreach (AudioSource audioSource in SFX)
+        SoundScript soundScript = Array.Find(musicClips, clip => clip.name == clipName);
+
+        //if (musicSource.isPlaying)
+        //{
+        //    musicSource.Stop();
+        //}
+        if (soundScript == null)
         {
-            audioSource.Stop();
+            Debug.LogError("SOUND NOT FOUND");
+        }
+        else
+        {
+            musicSource.clip = soundScript.audioClip;
+            musicSource.Play();
         }
     }
 
-    public void SetVolume(float volume)
+    public void PlaySFX(string clipName)
     {
-        foreach (AudioSource audioSource in SFX)
+        SoundScript soundScript = Array.Find(SFXClips, clip => clip.name == clipName);
+
+        //if (musicSource.isPlaying)
+        //{
+        //    musicSource.Stop();
+        //}
+        if (soundScript == null)
         {
-            audioSource.volume = volume;
+            Debug.LogError("SOUND NOT FOUND");
         }
+        else
+        {
+            sfxSource.PlayOneShot(soundScript.audioClip);
+        }
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
     }
 }
